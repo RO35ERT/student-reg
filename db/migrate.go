fdecd9fd01f50f7bc29590723eee7365eabbd413
RO35ERT <trebrorobertsbob@gmail.com>
2024-01-07 21:21:25 +0200

    fixed the update

--- db/migrate.go
+++ db/migrate.go
@@ -3,7 +3,6 @@
 package db
 
 import (
-	"os"
 	"student-api/models"
 
 	"gorm.io/driver/mysql"
@@ -11,7 +10,7 @@ import (
 )
 
 func Migrate() *gorm.DB {
-    dsn := os.Getenv("DB_USER")+":"+ os.Getenv("DB_PASS") +"@tcp(localhost:3306)/StudentDB?charset=utf8mb4&parseTime=True&loc=Local"
+    dsn := "root:tumbwerobert@tcp(localhost:3306)/StudentDB?charset=utf8mb4&parseTime=True&loc=Local"
     db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
     if err != nil {
         panic("Failed to connect to database")

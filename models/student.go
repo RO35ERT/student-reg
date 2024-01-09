639c81b235b7a5ab735f1b4a8117067fb68b0149
RO35ERT <98536689+RO35ERT@users.noreply.github.com>
2024-01-09 19:13:06 +0200

    Update student.go

--- models/student.go
+++ models/student.go
@@ -6,6 +6,6 @@ import "gorm.io/gorm"
 
 type Student struct {
     gorm.Model
-    Name  string `json:"name" gorm:"size:50;not null"`
+    Name  string `json:"name" gorm:"size:30;not null"`
     Email string `json:"email" gorm:"size:50;unique;not null"`
 }
